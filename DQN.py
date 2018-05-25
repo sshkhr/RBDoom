9a8b63d24c1fe28eb8518ef66411b8c5252df23b
Shashank Shekhar <sshekhar.special@gmail.com>
2018-05-25 12:40:30 +0530

    Added DQN and Replay Memory from examples

---
+++ DQN.py
@@ -0,0 +1,55 @@
+import numpy as np
+import skimage.color, skimage.transform
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from tqdm import trange
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        channels = 1
+        state_shape = (capacity, channels, resolution[0], resolution[1])
+        self.s1 = np.zeros(state_shape, dtype=np.float32)
+        self.s2 = np.zeros(state_shape, dtype=np.float32)
+        self.a = np.zeros(capacity, dtype=np.int32)
+        self.r = np.zeros(capacity, dtype=np.float32)
+        self.isterminal = np.zeros(capacity, dtype=np.float32)
+
+        self.capacity = capacity
+        self.size = 0
+        self.pos = 0
+
+    def add_transition(self, s1, action, s2, isterminal, reward):
+        self.s1[self.pos, 0, :, :] = s1
+        self.a[self.pos] = action
+        if not isterminal:
+            self.s2[self.pos, 0, :, :] = s2
+        self.isterminal[self.pos] = isterminal
+        self.r[self.pos] = reward
+
+        self.pos = (self.pos + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
+
+    def get_sample(self, sample_size):
+        i = sample(range(0, self.size), sample_size)
+        return self.s1[i], self.a[i], self.s2[i], self.isterminal[i], self.r[i]
+
+class Net(nn.Module):
+    def __init__(self, available_actions_count):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=6, stride=3)
+        self.conv2 = nn.Conv2d(8, 8, kernel_size=3, stride=2)
+        self.fc1 = nn.Linear(192, 128)
+        self.fc2 = nn.Linear(128, available_actions_count)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 192)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+criterion = nn.MSELoss()
