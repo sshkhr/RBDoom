9f932bed81be138ad49c3992787759d9f902c1bf
Shashank Shekhar <sshekhar.special@gmail.com>
2018-05-25 14:08:50 +0530

    Added simple replay memory

---
+++ Memory.py
@@ -0,0 +1,28 @@
+class ReplayMemory:
+    def __init__(self, capacity):
+        channels = 1
+        state_shape = (capacity, channels, resolution[0], resolution[1])
+        self.s1 = np.zeros(state_shape, dtype=np.float32)
+        self.s2 = np.zeros(state_shape, dtype=np.float32)
+        self.a = np.zeros(capacity, dtype=np.int32)
+        self.r = np.zeros(capacity, dtype=np.float32)
+        self.isterminal = np.zeros(capacity, dtype=np.float32)
+
+        self.capacity = capacity
+        self.size = 0
+        self.pos = 0
+
+    def add_transition(self, s1, action, s2, isterminal, reward):
+        self.s1[self.pos, 0, :, :] = s1
+        self.a[self.pos] = action
+        if not isterminal:
+            self.s2[self.pos, 0, :, :] = s2
+        self.isterminal[self.pos] = isterminal
+        self.r[self.pos] = reward
+
+        self.pos = (self.pos + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
+
+    def get_sample(self, sample_size):
+        i = sample(range(0, self.size), sample_size)
+        return self.s1[i], self.a[i], self.s2[i], self.isterminal[i], self.r[i]
